2e705ddf76c4e418f5e3054997fdd7757c4247e3
fengnanyue <fengnanyue@zju.edu.cn>
2015-09-14 15:17:18 -0600

    Add logical realization of GetCode function

--- app/src/main/java/atys/AtyLogin.java
+++ app/src/main/java/atys/AtyLogin.java
@@ -1,9 +1,16 @@
 package atys;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.fengnanyue.secretchat.R;
+
+import net.GetCode;
 
 /**
  * Created by Fernando on 15/9/13.
@@ -14,5 +21,34 @@ public class AtyLogin extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.aty_login);
+        etPhone = (EditText) findViewById(R.id.etPhoneNum);
+        findViewById(R.id.btnGetCode).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(TextUtils.isEmpty(etPhone.getText())){
+                    Toast.makeText(AtyLogin.this, R.string.phone_num_can_not_empty,Toast.LENGTH_LONG).show();
+                    return;
+
+                }
+
+                final ProgressDialog pd = ProgressDialog.show(AtyLogin.this,getResources().getString(R.string.connecting),getResources().getString(R.string.connecting_to_server));
+                new GetCode(etPhone.getText().toString(), new GetCode.SuccessCallBack() {
+                    @Override
+                    public void onSuccess() {
+                        pd.dismiss();
+                        Toast.makeText(AtyLogin.this, R.string.suc_to_get_code,Toast.LENGTH_LONG).show();
+
+                    }
+                }, new GetCode.FailCallBack() {
+                    @Override
+                    public void onFail() {
+                        pd.dismiss();
+                        Toast.makeText(AtyLogin.this, R.string.fail_to_get_code,Toast.LENGTH_LONG).show();
+
+                    }
+                });
+            }
+        });
     }
+    private EditText etPhone = null;
 }
--- app/src/main/java/com/fengnanyue/secretchat/MainActivity.java
+++ app/src/main/java/com/fengnanyue/secretchat/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import atys.AtyLogin;
 import atys.AtyTimeline;
 
 public class MainActivity extends Activity {
@@ -13,14 +14,14 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
 
         String token = Configure.getCachedToken(this);
-        startActivity(new Intent(this,AtyTimeline.class));
-//        if(token!=null){
-//            Intent i = new Intent(this, AtyTimeline.class);
-//            i.putExtra(Configure.KEY_TOKEN,token);
-//            startActivity(i);
-//        }else{
-//            startActivity(new Intent(this, AtyLogin.class));
-//        }
+//        startActivity(new Intent(this,AtyTimeline.class));
+        if(token!=null){
+            Intent i = new Intent(this, AtyTimeline.class);
+            i.putExtra(Configure.KEY_TOKEN,token);
+            startActivity(i);
+        }else{
+            startActivity(new Intent(this, AtyLogin.class));
+        }
 
         finish();
     }
