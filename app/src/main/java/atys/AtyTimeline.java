6bc5997bb4aba68570e872f60720df4f8f35d20b
fengnanyue <fengnanyue@zju.edu.cn>
2015-09-15 15:58:14 -0600

    logical realization of uploading messages

--- app/src/main/java/atys/AtyTimeline.java
+++ app/src/main/java/atys/AtyTimeline.java
@@ -1,9 +1,16 @@
 package atys;
 
 import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.fengnanyue.secretchat.Configure;
 import com.fengnanyue.secretchat.R;
+import com.fengnanyue.secretchat.tools.MD5Tool;
+
+import net.UploadContacts;
+
+import ld.MyContacts;
 
 /**
  * Created by Fernando on 15/9/13.
@@ -13,5 +20,30 @@ public class AtyTimeline extends ListActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.aty_timeline);
+
+        phone_num = getIntent().getStringExtra(Configure.KEY_PHONE_NUM);
+        token = getIntent().getStringExtra(Configure.KEY_TOKEN);
+        phone_md5 = MD5Tool.md5(phone_num);
+
+        new UploadContacts(phone_md5, token, MyContacts.getContactsJSONString(this), new UploadContacts.SuccessCallBack() {
+            @Override
+            public void onSuccess() {
+                loadMessage();
+            }
+        }, new UploadContacts.FailCallBack() {
+            @Override
+            public void onFail(int errorCode) {
+                if(errorCode==Configure.RESULT_STATUS_INVALID_TOKEN){
+                    startActivity(new Intent(AtyTimeline.this,AtyLogin.class));
+                    finish();
+                }else{
+                    loadMessage();
+                }
+            }
+        });
     }
+    private void loadMessage(){
+        System.out.println(">>>>>>>>>>>>>>>>>>>>LoadMessage");
+    }
+    private String phone_num,token,phone_md5;
 }
--- app/src/main/java/com/fengnanyue/secretchat/MainActivity.java
+++ app/src/main/java/com/fengnanyue/secretchat/MainActivity.java
@@ -1,7 +1,11 @@
 package com.fengnanyue.secretchat;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+
+import atys.AtyLogin;
+import atys.AtyTimeline;
 
 public class MainActivity extends Activity {
 
@@ -11,16 +15,19 @@ public class MainActivity extends Activity {
 
 //        System.out.println(MyContacts.getContactsJSONString(this));
 
-//        String token = Configure.getCachedToken(this);
-//        if(token!=null){
-//            Intent i = new Intent(this, AtyTimeline.class);
-//            i.putExtra(Configure.KEY_TOKEN,token);
-//            startActivity(i);
-//        }else{
-//            startActivity(new Intent(this, AtyLogin.class));
-//        }
-//
-//        finish();
+        String token = Configure.getCachedToken(this);
+        String phone_num = Configure.getCachedPhoneNumber(this);
+
+        if(token!=null&&phone_num!=null){
+            Intent i = new Intent(this, AtyTimeline.class);
+            i.putExtra(Configure.KEY_TOKEN,token);
+            i.putExtra(Configure.KEY_PHONE_NUM,phone_num);
+            startActivity(i);
+        }else{
+            startActivity(new Intent(this, AtyLogin.class));
+        }
+
+        finish();
     }
 
 
